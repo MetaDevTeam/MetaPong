d2216f6103b5eb544cef3e4e1a77eb81091de07e
dratuz <dr.vi.mv@gmail.com>
2017-04-11 21:19:31 +0300

    First cimmit console Window Width and Height; Commit test @zluzunov

--- MetaPong/Startup.cs
+++ MetaPong/Startup.cs
@@ -10,6 +10,8 @@ namespace MetaPong
     {
         static void Main(string[] args)
         {
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
         }
     }
 }
