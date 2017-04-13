02b9fa1f8e88386dafb1870f4798cee3937df36e
Zlatyo Uzunov <zluzunov@gmail.com>
2017-04-13 16:27:25 +0300

    added utilities class library and the label class

---
+++ MetaPong.Utilities/ScreenElements/Label.cs
@@ -0,0 +1,26 @@
+﻿namespace MetaPong.Utilities.ScreenElements
+{
+    using System;
+
+    /// <summary>
+    /// A class for printing text lables in the console.
+    /// It can be used for the TUI buttons, points and tex elements
+    /// </summary>
+    /// <seealso cref="MetaPong.Utilities.ScreenElements.ScreenElement" />
+    public class Label: ScreenElement
+    {
+        private readonly string _content;
+
+        public Label(int x, int y, string content) 
+            : base(x, y)
+        {
+            _content = content;
+        }
+
+        public override void Print()
+        {
+            Console.SetCursorPosition(_x,_y);
+            Console.Write(_content);
+        }
+    }
+}
---
+++ MetaPong.Utilities/ScreenElements/ScreenElement.cs
@@ -0,0 +1,18 @@
+﻿namespace MetaPong.Utilities.ScreenElements
+{
+    using System;
+
+    public abstract class ScreenElement
+    {
+        protected int _x;
+        protected int _y;
+
+        public ScreenElement(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public abstract void Print();
+    }
+}
