daec31945921cf11b8e3f662ba58729de86ef841
Zlatyo Uzunov <zluzunov@gmail.com>
2017-04-13 18:22:33 +0300

    Revert "Added interface demo;"
    This reverts commit 7796ddb05d57ad63b9715abb5d6941ca80229ec9.

--- MetaPong/Startup.cs
+++ MetaPong/Startup.cs
@@ -1,42 +1,17 @@
-﻿namespace MetaPong
-{
-    using System;
-    using Utilities.ScreenElements;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace MetaPong
+{
     class Startup
     {
         static void Main(string[] args)
         {
-            RunInterfaceDemo();
-        }
-
-        private static void RunInterfaceDemo()
-        {
-// Setup screen
-            int windowWidth = Console.LargestWindowWidth - 5;
-            int windowHeight = Console.LargestWindowHeight - 5;
-
-
-            Console.BufferWidth = windowWidth;
-            Console.BufferHeight = windowHeight;
-            Console.WindowWidth = windowWidth;
-            Console.WindowHeight = windowHeight;
-
-            var optionsScreen = new ScreenGroup();
-
-            var startColumn = (windowWidth / 2) - 5;
-            var startRow = (windowHeight / 2) - 3;
-
-            optionsScreen.Add(new ScreenLable(startRow, startColumn, "1 PLAYER"));
-            optionsScreen.Add(new ScreenLable(startRow + 1, startColumn, "2 PLAYERS"));
-            optionsScreen.Add(new ScreenLable(startRow + 2, startColumn, "OPTIONS"));
-            optionsScreen.Add(new ScreenLable(startRow + 3, startColumn, "SAVE GAME"));
-            optionsScreen.Add(new ScreenLable(startRow + 4, startColumn, "LOAD GAME"));
-            optionsScreen.Add(new ScreenLable(startRow + 5, startColumn, "EXIT"));
-
-            optionsScreen.Print();
-
-            Console.ReadKey(true);
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
         }
     }
 }
