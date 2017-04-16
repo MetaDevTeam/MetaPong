04660c10bf08ba56659711831454cc9b68602a18
dratuz <dr.vi.mv@gmail.com>
2017-04-16 20:01:20 +0300

    Added Project MetaPong.DataModels Added Two models for Database.

---
+++ MetaPong.DataModels/Models/GameResult.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MetaPong.DataModels.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GameResult
+    {
+        public enum GameResults
+        {
+            Win,
+            Lose
+        }
+
+        public int Id { get; set; }
+
+        
+        public GameResults Result { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ MetaPong.DataModels/Models/User.cs
@@ -0,0 +1,35 @@
+﻿
+namespace MetaPong.DataModels.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class User
+    {
+        public User()
+        {
+            this.GameResults = new HashSet<GameResult>();
+        }
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        //TODO: have to count results for win
+        public int? WinCount { get; set; }
+
+        //TODO: have to count results for lose
+        public int? LoseCount { get; set; }
+
+        public virtual ICollection<GameResult> GameResults { get; set; }
+    }
+}
