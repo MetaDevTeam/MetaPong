e5a8078d0c423a5b3dbbd8eae7a1eaa9eabdabdd
Zlatyo Uzunov <zluzunov@gmail.com>
2017-04-17 10:53:08 +0300

    some initial ideas about the player class refactoring; TODO: finish and test the class (unit tests?)

---
+++ MetaPong/PongElements/DrawElements/PlayerO.cs
@@ -0,0 +1,38 @@
+﻿namespace MetaPong.PongElements.DrawElements
+{
+    using System;
+    using Utilities;
+    using Utilities.ScreenElements;
+
+
+    public class PlayerO
+    {
+        private const int Thickness = 2;
+        private const int Height = 8;
+        private int _row = 0;
+
+        private ScreenLayout _layout;
+
+        public PlayerO(int row, int size, string side)
+        {
+            _row = row;
+            _layout = Composer.GetBox(Thickness, Height, _row, GetSide(side));
+        }
+
+        private int GetSide(string side)
+        {
+            switch (side)
+            {
+                case "Right":
+                    return 130 - Thickness;
+                default:
+                    return 0;
+            }
+        }
+
+        public void Print()
+        {
+            _layout.Print();
+        }
+    }
+}
